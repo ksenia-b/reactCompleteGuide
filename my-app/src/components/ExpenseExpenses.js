5936fb9bd51b821a1a83bf727edd75137d6a0df9
ksenia-b <ksenia.barshchyk@gmail.com>
2022-10-29 12:39:18 +0300

    added new components

--- my-app/src/components/ExpenseExpenses.js
+++ my-app/src/components/ExpenseExpenses.js
@@ -1,16 +1,17 @@
 import ExpenseItem from './ExpenseItem';
 import './ExpenseExpenses.css';
+import Card from './Card';
 
 function ExpenseExpenses(props){
    console.log("props here are = ", props)
-        return (<div className='expenses'>
+        return (<Card className='expenses'>
             <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}
             expenses={props.expenses[0].expenses}></ExpenseItem>
           <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount}  date={props.expenses[1].date}
             expenses={props.expenses[1].expenses}></ExpenseItem>
             <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount}  date={props.expenses[2].date}
             expenses={props.expenses[2].expenses}></ExpenseItem>
-            </div> );
+            </Card> );
 }
 
 export default ExpenseExpenses;
